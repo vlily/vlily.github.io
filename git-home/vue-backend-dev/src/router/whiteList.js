d52a8b8dcb9fe39dfec7fbc55f5a0ce3c3edf6a3
vlily <976651023@qq.com>
2020-04-09 14:59:46 +0800

    sorry

--- git-home/vue-backend-dev/src/router/whiteList.js
+++
@@ -1,9 +0,0 @@
-// 免登录白名单页面
-const whiteList = [
-    '/login',
-    '/register',
-    '/notice',
-    '/maintenance'
-]
-
-export default whiteList
--- git-home/vue-backend-dev/src/store/modules/index.js
+++
@@ -1,9 +0,0 @@
-import user from './user'
-import auth from './auth'
-import tagNav from './tagNav'
-
-export default {
-    user: user,
-    auth: auth,
-    tagNav: tagNav
-}
